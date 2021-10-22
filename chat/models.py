14ca7ebdbe246d83d3ae22be45e91ac53461d740
kryptwon <kingahoora23@gmail.com>
2021-10-22 23:51:58 +0330

    chat room debuged

--- chat/models.py
+++ chat/models.py
@@ -6,7 +6,7 @@ class RoomMessageModel(models.Model):
 
   creator = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
   name = models.CharField(max_length=200, unique=True, null=True)
-  message = JSONField(null=True)
+  message = JSONField(default={"message":[]})
 
   def __str__(self) -> str:
     return str(self.name)
